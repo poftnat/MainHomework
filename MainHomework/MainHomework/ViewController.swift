413600067f91421943e0618924e171236fc12b3e
poftnat <149266332+poftnat@users.noreply.github.com>
2023-11-11 22:38:35 +0300

    create main

---
+++ MainHomework/MainHomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MainHomework
+//
+//  Created by Наталья Владимировна Пофтальная on 11.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
