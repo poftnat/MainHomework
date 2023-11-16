92a8e314c4a4dc17a3e6bef05870d17b2f685ef4
poftnat <149266332+poftnat@users.noreply.github.com>
2023-11-17 01:25:46 +0300

    1st task

---
+++ MainHomework/MainHomework/FourOfTaskOneViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FourOfTaskOneViewController.swift
+//  MainHomework
+//
+//  Created by Наталья Владимировна Пофтальная on 16.11.2023.
+//
+
+import UIKit
+
+class FourOfTaskOneViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
